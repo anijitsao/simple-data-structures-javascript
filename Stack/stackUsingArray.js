ff8def245080cd6c1b6e6a2943ccc958a4bb3da2
anijit <anijitsau@gmail.com>
2022-07-11 18:28:38 +0530

    stack unit test cases complete

--- Stack/stackUsingArray.js
+++ Stack/stackUsingArray.js
@@ -1,5 +1,5 @@
 // this example shows how to implement Stack Data Structure
-// using Array.
+// using Array. Most common operations using Stack is covered
 
 export default class Stack {
   // initialize the Stack
@@ -8,7 +8,7 @@ export default class Stack {
     this.size = 0;
   }
 
-  // PUSH opertaion LIFO
+  // PUSH opertaion
   push(ele) {
     this.elements = [...this.elements, ele];
     this.size = this.size + 1;
@@ -16,21 +16,25 @@ export default class Stack {
 
   // POP operation LIFO
   pop() {
+    if (this.size === 0) {
+      return false;
+    }
     this.elements.length = this.elements.length - 1;
     this.size = this.size - 1;
   }
 
   // print the current Stack
   print() {
-    if (this.size === 0) {
-      return "This Stack is empty";
-    } else {
-      return this.elements.toString();
-    }
+    return this.elements.toString();
   }
 
   // get size of Stack
   getSize() {
     return this.size;
+  }
+
+  // get the index of a searched value
+  search(value) {
+    return this.elements.lastIndexOf(value);
   }
 }
--- Stack/stackUsingArray.test.js
+++ Stack/stackUsingArray.test.js
@@ -10,6 +10,37 @@ describe("=============== Testing Stack Data Structure with Jest ===============
   });
 
   test("Printing the Empty Stack", () => {
-    expect(stackTest.print()).toBe("This Stack is empty");
+    expect(stackTest.print()).toEqual("");
+  });
+
+  test("Pushing elements in the Stack", () => {
+    stackTest.push(10);
+    expect(stackTest.print()).toEqual("10");
+  });
+
+  test("Pushing element and checking size of the Stack", () => {
+    expect(stackTest.getSize()).toBe(1);
+  });
+
+  test("Popping elements from the Stack", () => {
+    stackTest.pop();
+    expect(stackTest.print()).toEqual("");
+  });
+
+  test("Popping elements and checking size of the Stack", () => {
+    expect(stackTest.getSize()).toBe(0);
+  });
+
+  test("Popping elements from an Empty Stack", () => {
+    const emptyPop = stackTest.pop();
+    expect(emptyPop).toBe(false);
+  });
+
+  test("Searching for a value", () => {
+    stackTest.push(10);
+    stackTest.push(234);
+    stackTest.push(10);
+    stackTest.push(99);
+    expect(stackTest.search(10)).toBe(2);
   });
 });
