1711b57187d6994abc4bcbd1bf85eeb8c9f16c00
anijit <anijitsau@gmail.com>
2022-07-11 13:18:42 +0530

    test case adding started

---
+++ Stack/stackUsingArray.test.js
@@ -0,0 +1,9 @@
+describe("=============== testing with Jest ===============", () => {
+  test("1 + 1 returns 2", () => {
+    let a = 1;
+    let b = 1;
+    let c = a + b;
+
+    expect(c).toBe(2);
+  });
+});
