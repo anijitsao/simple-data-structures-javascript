b3346840b7931c3fc5fbc8d591670d6a925d7304
anijit <anijitsau@gmail.com>
2022-12-14 13:10:15 +0530

    findMinimum with typecript added

---
+++ Arrays/findMinimum.ts
@@ -0,0 +1,13 @@
+// the following code find minimum of an array
+// using Typescript
+function findMinimum(arrReceived: number[]) {
+  let minIndex = 0;
+  for (let i = 0; i < arrReceived.length; i++) {
+    if (arrReceived[minIndex] > arrReceived[i]) {
+      minIndex = i;
+    }
+  }
+  return arrReceived[minIndex];
+}
+
+console.log("Minimum: ", findMinimum([110, 2, 34, 22, 4]));
