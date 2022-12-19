38f2b47026e349ca5ccf408d9af0ac1a3361b386
anijit <anijitsau@gmail.com>
2022-12-19 12:59:23 +0530

    linked list complete

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -1,7 +1,7 @@
 // a Node in the Linked List
-class LinkedListNode {
+class Node {
   constructor(value) {
-    this.data = value;
+    this.element = value;
     this.next = null;
   }
 }
@@ -9,37 +9,68 @@ class LinkedListNode {
 class LinkedList {
   constructor() {
     this.head = null;
-    this.tail = null;
+    this.length = 0;
   }
 
   append(value) {
     // first create a node with the value supplied and then append it
-    let nodeNew = new LinkedListNode(value);
+    let nodeToInsert = new Node(value);
 
     // if the list is empty
     if (this.head === null) {
-      this.head = nodeNew;
-      this.tail = nodeNew;
-    }
-
-    // if the list has some nodes
-    if (this.head) {
+      this.head = nodeToInsert;
+      this.length = this.length + 1;
+    } else {
+      // if the list is not empty
       let lastNode = this.head;
       while (lastNode.next) {
         lastNode = lastNode.next;
       }
-      lastNode.next = nodeNew;
-      this.tail = nodeNew;
+      lastNode.next = nodeToInsert;
+      this.length = this.length + 1;
     }
+  }
+
+  getLength() {
+    return this.length;
+  }
+
+  search(element) {
+    let currentNode = this.head;
+    // for the first node
+    if (currentNode.element === element) {
+      return true;
+    }
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      if (currentNode.element === element) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  printList() {
+    const listElements = [this.head.element];
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      listElements.push(currentNode.element);
+    }
+    return listElements.toString();
   }
 }
 
 const list1 = new LinkedList();
 console.log("list", list1);
+list1.append(37);
+console.log("list", list1);
+list1.append(38);
+console.log("list", list1);
+console.log(`list has ${list1.getLength()} elements`);
 
-list1.append(24);
-console.log("List is", list1);
-list1.append(45);
-console.log("List", list1);
-list1.append(57);
-console.log("List", list1);
+console.log("does the list has element", 38, list1.search(38));
+console.log("does the list has element", 37, list1.search(37));
+console.log("does the list has element", 35, list1.search(35));
+
+console.log("print list", list1.printList());
