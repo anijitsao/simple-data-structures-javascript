52d6bf27057df3628acea82d42724216f6b19905
anijit <anijitsau@gmail.com>
2023-01-03 13:31:15 +0530

    post verb added

--- Others/Nodejs-supertest/server.js
+++ Others/Nodejs-supertest/server.js
@@ -2,8 +2,17 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World" });
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).json({ message: "fields missing" });
+  }
+  res.status(200).json({ message: "login success" });
 });
 
 // To run the server uncomment the following lines
